1e873a62636a0f62b597a7d389aee415433c0bd5
James Maskell <jameslmaskell@gmail.com>
2020-08-17 12:57:52 +0100

    All tests pass. Trying to make the UX good

--- barchart.js
+++ barchart.js
@@ -1,28 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-	console.log("We are now alive");
-
 	containerHeight = window.innerHeight - 200;
 	containerWidth = window.innerWidth - 200;
 	containerPadding = 50;
 
 	getData().then((data) => {
 		let container = createContainer(containerHeight, containerWidth);
-		let scaleArray = createScales(
-			data.data,
-			containerHeight,
-			containerWidth,
-			containerPadding
-		);
+		let scaleArray = createScales(data.data, containerHeight, containerWidth, containerPadding);
 		createAxis(scaleArray, containerHeight, containerPadding);
-		createBars(
-			scaleArray,
-			data.data,
-			containerHeight,
-			containerWidth,
-			containerPadding
-		);
+		createBars(scaleArray, data.data, containerHeight, containerWidth, containerPadding);
 	});
 });
+
+function createContainer(containerHeight, containerWidth) {
+	d3.select("body").append("svg").attr("width", containerWidth).attr("height", containerHeight);
+}
 
 function createAxis(scaleArray, containerHeight, containerPadding) {
 	let xScale = scaleArray[0];
@@ -39,67 +30,36 @@ function createAxis(scaleArray, containerHeight, containerPadding) {
 	d3.select("svg")
 		.append("g")
 		.attr("id", "x-axis")
-		.attr(
-			"transform",
-			"translate(0," + (containerHeight - containerPadding) + ")"
-		)
+		.attr("transform", "translate(0," + (containerHeight - containerPadding) + ")")
 		.call(xAxis);
 }
 
 function getData() {
-	let url =
-		"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+	let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 	return fetch(url).then((response) => response.json());
 }
 
-function createScales(
-	dataset,
-	containerHeight,
-	containerWidth,
-	containerPadding
-) {
+function createScales(dataset, containerHeight, containerWidth, containerPadding) {
 	let yMax = d3.max(dataset, (d) => d[1]);
 	let yScale = d3.scaleLinear();
-	yScale
-		.domain([0, yMax])
-		.range([containerHeight - containerPadding, containerPadding]);
+	yScale.domain([0, yMax]).range([containerHeight - containerPadding, containerPadding]);
 
 	let xScale = d3.scaleTime();
-
-	console.log(d3.min(dataset, (d) => d[0]));
-
 	let xMin = d3.min(dataset, (d) => d[0]);
 	let xMax = d3.max(dataset, (d) => d[0]);
-
-	xScale
-		.domain([new Date(xMin), new Date(xMax)])
-		.range([containerPadding, containerWidth - containerPadding]);
+	let date = new Date(xMax);
+	date.setDate(date.getDate() + 92);
+	xScale.domain([new Date(xMin), date]).range([containerPadding, containerWidth - containerPadding]);
 
 	return [xScale, yScale];
 }
 
-function createContainer(containerHeight, containerWidth) {
-	d3.select("body")
-		.append("svg")
-		.attr("width", containerWidth)
-		.attr("height", containerHeight);
-}
-
-function createBars(
-	scaleArray,
-	dataset,
-	containerHeight,
-	containerWidth,
-	containerPadding
-) {
+function createBars(scaleArray, dataset, containerHeight, containerWidth, containerPadding) {
 	let xScale = scaleArray[0];
 	let yScale = scaleArray[1];
 
-	let columnWidth = (containerWidth - containerPadding * 2) / dataset.length;
-	let barWidth = columnWidth - 2;
-
-	console.log(xScale(243.1));
-	console.log(xScale(14000));
+	let barWidth = (containerWidth - containerPadding * 2) / dataset.length + 0.5;
+	let xAxisYPosition = containerHeight - containerPadding;
 
 	d3.select("svg")
 		.selectAll("rect")
@@ -109,11 +69,44 @@ function createBars(
 		.attr("class", "bar")
 		.attr("data-date", (d) => d[0])
 		.attr("data-gdp", (d) => d[1])
-		.attr("width", 3)
-		.attr("height", (d) => containerHeight - yScale(d[1]) - containerPadding)
+		.attr("width", barWidth)
+		.attr("height", (d) => xAxisYPosition - yScale(d[1]))
 		.attr("x", (d) => xScale(new Date(d[0])))
 		.attr("y", (d) => yScale(d[1]))
 		.attr("fill", "blue")
-		.append("title")
-		.text((d) => d[0]);
+		.on("mouseover", function (d, i) {
+			addTooltipBox(xScale, yScale, d);
+			addTooltipText(this, xScale, yScale, d, containerWidth);
+		})
+		.on("mouseout", removeTooltip);
+}
+
+function addTooltipBox(xScale, yScale, d, containerWidth) {
+	console.log(d);
+	d3.select("svg")
+		.append("rect")
+		.attr("id", "tooltip-box")
+		.attr("x", xScale(new Date(d[0])) - 231)
+		.attr("y", yScale(d[1]) - 50)
+		.attr("width", 231)
+		.attr("height", 50)
+		.attr("fill", "yellow");
+}
+
+function addTooltipText(e, xScale, yScale, d, containerWidth) {
+	d3.select(e).attr("fill", "red");
+
+	if (xScale(new Date(d[0])) - 116 + 231 > containerWidth) {
+		tooltipXPos = containerWidth - 231;
+	} else {
+		tooltipXPos = xScale(new Date(d[0])) - 116;
+	}
+
+	d3.select("svg").append("text").text(`Quarter: ${d[0]} GDP: ${d[1]}`).attr("id", "tooltip").attr("x", tooltipXPos).attr("y", yScale(d[1])).attr("data-date", d[0]).attr("data-gdp", d[1]);
+}
+
+function removeTooltip(d, i) {
+	d3.select(this).attr("fill", "blue");
+	d3.select("#tooltip").remove();
+	d3.select("#tooltip-box").remove();
 }
